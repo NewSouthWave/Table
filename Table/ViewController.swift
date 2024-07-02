efdb5d10bc92115817983facca2b295cc049b6e9
NewSouthWave <tigers0110@naver.com>
2024-07-02 16:30:44 +0900

    Initial Commit

---
+++ Table/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Table
+//
+//  Created by Nam on 2024/07/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
